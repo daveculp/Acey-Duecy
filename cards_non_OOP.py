7772f9c9acb3280f5f6ab39089de4bd6ac3e05ba
daveculp <dwculp@gmail.com>
2021-10-12 21:02:25 -0500

    Update cards_non_OOP.py

--- cards_non_OOP.py
+++ cards_non_OOP.py
@@ -1,4 +1,5 @@
 import random
+
 def show(deck):
 	for card in deck:
 		print( '{0} of {1}'.format( card_values [card[0]], card[1]) )
@@ -14,7 +15,7 @@ def choose_random(deck, discards, remove = True):
 	if remove:
 		deck.remove(card)
 		deck.append(card)
-	return card
+	return card  
 	
 def get_next(deck, discards, remove = True):
 	if len(deck) == 0:
@@ -31,6 +32,7 @@ discards = []
 
 suits = ("Clubs", "Diamonds","Hearts", "Spades")
 card_values = {2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10",11:"Jack",12:"Queen", 13:"King", 14:"Ace"}
+
 for suit in range (0,4):
 	for val in range(2,15):
 		card = (val, suits[suit])
